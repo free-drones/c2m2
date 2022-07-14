eba9ac739c49291a19369cb9027dd1b20994cb0f
hx-003 <rise.hx003@gmail.com>
2022-07-14 11:39:30 +0000

    Added opencv-based camera class

---
+++ dronenet/camera.py
@@ -0,0 +1,22 @@
+import cv2
+from dronenet.base_camera import BaseCamera
+
+
+class Camera(BaseCamera):
+    video_source = ''
+    def __init__(self, source):
+        Camera.video_source = source
+        super(Camera, self).__init__()
+
+    @staticmethod
+    def frames():
+        camera = cv2.VideoCapture(Camera.video_source)
+        if not camera.isOpened():
+            raise RuntimeError('Could not start camera.')
+
+        while True:
+            # read current frame
+            _, img = camera.read()
+
+            # encode as a jpeg image and return it
+            yield cv2.imencode('.jpg', img)[1].tobytes()
